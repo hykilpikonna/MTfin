32706ca28fef43e9b712123404fec817865fdeec
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2026-03-10 16:47:35 -0400

    [F] Fix getting response

--- utils_ai.py
+++ utils_ai.py
@@ -37,7 +37,7 @@ def select_best_torrents(torrents_text: str) -> str:
         store=True,
         include=["reasoning.encrypted_content", "web_search_call.action.sources"]
     )
-    assert (output := response.output[-1]).type == "output_text"
+    assert (output := response.output[-1]).type == "message"
     assert len(contents := output.content) == 1
     return contents[0].text
 
@@ -73,7 +73,7 @@ def generate_rename_mapping(directory_text: str) -> dict[str, str]:
         store=True,
         include=["reasoning.encrypted_content", "web_search_call.action.sources"]
     )
-    assert (output := response.output[-1]).type == "output_text"
+    assert (output := response.output[-1]).type == "message"
     assert len(contents := output.content) == 1
     raw_response = contents[0].text
     
